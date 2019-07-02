9d80b24e1bf293fc1506752d399785eecb0aa138
Matthew Kosloski <matthewkosloski@me.com>
2019-07-02 12:52:37 -0500

    added log function for debug mode

--- server/modules/SFDCClient.js
+++ server/modules/SFDCClient.js
@@ -7,14 +7,15 @@ require('cometd-nodejs-client').adapt();
  */
 class SFDCClient {
 
-    constructor(cometd, jsforce, username, password, apiVersion) {
-        console.log('Creating a new instance of SFDCClient for a socket.');
+    constructor(cometd, jsforce, username, password, apiVersion, isDebugMode = false) {
+        this._log('Creating a new instance of SFDCClient for a socket.');
 
         this._cometd = cometd;
         this._jsforce = jsforce;
         this._username = username;
         this._password = password;
         this._apiVersion = apiVersion;
+        this._isDebugMode = isDebugMode;
         this._didShakeHands = false;
         this._subscriptions = {};
     }
@@ -31,7 +32,7 @@ class SFDCClient {
      * @throws Will throw an Error if unable to subscribe to channel.
      */
     async subscribe(channel, callback, subscribeCallback) {
-        console.log('SFDCClient.subscribe');
+        this._log('SFDCClient.subscribe');
 
         const didNotShakeHands = !this._didShakeHands;
         const hasNoSubscription = !this.hasSubscription(channel);
@@ -54,7 +55,7 @@ class SFDCClient {
      * an unsubscription occurs.
      */
     unsubscribe(channel, unsubscribeCallback) {
-        console.log('SFDCClient.unsubscribe');
+        this._log('SFDCClient.unsubscribe');
 
         if(this.hasSubscription(channel)) {
             const subscription = this._subscriptions[channel];
@@ -74,7 +75,7 @@ class SFDCClient {
      * @param {function} disconnectCallback Function to be invoked to acknowledge disconnect.
      */
     disconnect(disconnectCallback) {
-        console.log('SFDCClient.disconnect');
+        this._log('SFDCClient.disconnect');
         this._cometd.disconnect(disconnectCallback);
     }
 
@@ -84,6 +85,7 @@ class SFDCClient {
      * @param {string} channel The channel to use for the subscription check. 
      */
     hasSubscription(channel) {
+        this._log('SFDCClient.hasSubscription');
         return this._subscriptions[channel] !== undefined;
     }
 
@@ -93,6 +95,7 @@ class SFDCClient {
      * @throws Will throw an Error if unable to shake hands with CometD server.
      */
     async _handshake() {
+        this._log('SFDCClient._handshake');
         await this._configureCometD();
 
         await this._cometd.handshake(({successful}) => {
@@ -101,7 +104,7 @@ class SFDCClient {
                 throw new Error('Failed to shake hands with the Salesforce CometD server.');
             }
 
-            console.log('Successfully shook hands with the Salesforce CometD server.');
+            this._log('Successfully shook hands with the Salesforce CometD server.');
         });
 
         this._didShakeHands = true;
@@ -113,6 +116,7 @@ class SFDCClient {
      * @private Should only be used by members of the class.
      */
     async _configureCometD() {
+        this._log('SFDCClient._configureCometD');
         const { accessToken, instanceUrl } = await this._login();
         this._cometd.configure({
             url: `${instanceUrl}/cometd/${this.apiVersion}/`,
@@ -133,18 +137,29 @@ class SFDCClient {
      * 
      */
     async _login() {
+        this._log('SFDCClient._login');
         const { _username, _password } = this;
 
         await this._jsforce.login(_username, _password, (err) => {
             if (err) { 
-                console.log('Failed to log into Salesforce.');
+                this._log('Failed to log into Salesforce.');
                 throw new Error(err);
             }
 
-            console.log('Successfully logged into Salesforce.');
+            this._log('Successfully logged into Salesforce.');
         });
 
         return this._jsforce;
+    }
+
+    /**
+     * Logs a message to the console if in debug mode.
+     * @param {any} message The message to log to console. 
+     */
+    _log(message) {
+        if(this._isDebugMode) {
+            console.log(message);
+        }
     }
 
 }
