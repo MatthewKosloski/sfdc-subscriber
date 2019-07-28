8cb80d5af88e10002d5d13e6f98c72f498f762e3
Matthew Kosloski <matthewkosloski@me.com>
2019-07-28 15:57:50 -0500

    simplified button exports

--- client/src/design-system/theme/btn.ts
+++ client/src/design-system/theme/btn.ts
@@ -1,36 +1,17 @@
 import colors from './colors';
 
-export enum Button {
-	Default = 'default',
-	Primary = 'primary',
-	Danger = 'danger',
-	DangerLight = 'dangerLight',
-	Success = 'success',
-	SuccessLight = 'successLight'
+export const normal = {backgroundColor: colors.neutralBlack, color: colors.white};
+export const primary = {backgroundColor: colors.pantone306, color: colors.white};
+export const danger = {backgroundColor: colors.valencia, color: colors.white};
+export const dangerLight = {backgroundColor: colors.cosmos, color: colors.valencia};
+export const success = {backgroundColor: colors.eucalyptus, color: colors.white};
+export const successLight = {backgroundColor: colors.snowyMint, color: colors.eucalyptus};
+
+export default {
+	normal,
+	primary,
+	danger,
+	dangerLight,
+	success,
+	successLight
 };
-
-// export type Button = 'default' |
-// 	'primary' |
-// 	'danger' |
-// 	'dangerLight' |
-// 	'success' |
-// 	'successLight';
-
-
-type Btn = {
-	[key in Button]: {
-		// TODO: change value of key from type string to something more strict
-		[key in 'backgroundColor' | 'color']: string
-	}
-};
-
-const btn: Btn = {
-	default: {backgroundColor: colors.neutralBlack, color: colors.white},
-	primary: {backgroundColor: colors.pantone306, color: colors.white},
-	danger: {backgroundColor: colors.valencia, color: colors.white},
-	dangerLight: {backgroundColor: colors.cosmos, color: colors.valencia},
-	success: {backgroundColor: colors.eucalyptus, color: colors.white},
-	successLight: {backgroundColor: colors.snowyMint, color: colors.eucalyptus}
-};
-
-export default btn;
