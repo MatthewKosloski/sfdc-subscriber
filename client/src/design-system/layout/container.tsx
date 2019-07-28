6f320cf0562edb7254549d9b2845a84750820b73
Matthew Kosloski <matthewkosloski@me.com>
2019-07-28 15:38:42 -0500

    added containerWidth variable to theme

--- client/src/design-system/layout/container.tsx
+++ client/src/design-system/layout/container.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import { containerWidth } from '../theme/layout';
 import { spacingRem } from '../abstracts/mixins';
 
 interface IContainerProps {};
 
 const container = styled.div<IContainerProps>`
     ${spacingRem(['PL', 'PR'], 'One')};
-    width: 100%;
+    width: ${containerWidth};
     max-width: 100%;
     margin-left: auto;
     margin-right: auto;
--- client/src/design-system/theme/layout.ts
+++ client/src/design-system/theme/layout.ts
@@ -9,3 +9,5 @@ export enum Breakpoint {
 export type BreakpointStrings = keyof typeof Breakpoint;
 export type ColumnWidths = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type ColumnSizes = [BreakpointStrings, ColumnWidths][];
+
+export const containerWidth: string = '100%';
