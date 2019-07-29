c92510025c779d1b24a0be2cce3ad4642e613deb
Matthew Kosloski <matthewkosloski@me.com>
2019-07-29 00:04:12 -0500

    created theme and global style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Container from './design-system/layout/container';
 import { Row, Column } from './design-system/layout/grid';
-
 import Badge from './design-system/components/atoms/Badge';
+import theme from './theme';
+
+import GlobalStyle from './components/GlobalStyle';
 
 const App: React.FC = () => {
 	return(
-		<Container>
-			<Row>
-				<Column sizes={[['XS', 12], ['SM', 6]]}>
-					<h2>Badges</h2>
+		<Fragment>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Container>
 					<Row>
-						<Column>
-							<Badge small>Normal</Badge>
-							<Badge variant="primary" small>Primary</Badge>
-							<Badge variant="danger" small>Danger</Badge>
-							<Badge variant="dangerLight" small>Danger (Light)</Badge>
-							<Badge variant="success" small>Success</Badge>
-							<Badge variant="successLight" small>Success (Light)</Badge>
+						<Column sizes={[['XS', 12], ['SM', 6]]}>
+							<h2>Badges</h2>
+							<Row>
+								<Column>
+									<Badge small>Normal</Badge>
+									<Badge variant="primary" small>Primary</Badge>
+									<Badge variant="danger" small>Danger</Badge>
+									<Badge variant="dangerLight" small>Danger (Light)</Badge>
+									<Badge variant="success" small>Success</Badge>
+									<Badge variant="successLight" small>Success (Light)</Badge>
+								</Column>
+							</Row>
+						</Column>
+						<Column sizes={[['XS', 12], ['SM', 6]]}>
+							<h2>Buttons</h2>
 						</Column>
 					</Row>
-				</Column>
-				<Column sizes={[['XS', 12], ['SM', 6]]}>
-					<h2>Buttons</h2>
-				</Column>
-			</Row>
-		</Container>
+				</Container>
+			</ThemeProvider>
+		</Fragment>
 	);
 };
 
