36f8d7677bf8244444b7fb7523367189abe9a724
Matthew Kosloski <matthewkosloski@me.com>
2019-07-29 12:39:24 -0500

    updated app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,15 @@ import { ThemeProvider } from 'styled-components';
 
 import Container from './design-system/layout/container';
 import { Row, Column } from './design-system/layout/grid';
-import Badge from './design-system/components/atoms/Badge';
 import theme from './theme';
 
 import GlobalStyle from './components/GlobalStyle';
+
+import Badge from './design-system/components/atoms/Badge';
+import Button from './design-system/components/atoms/Button';
+import Label from './design-system/components/atoms/Label';
+import TextInput from './design-system/components/atoms/TextInput';
+
 
 const App: React.FC = () => {
 	return(
@@ -30,6 +35,23 @@ const App: React.FC = () => {
 						</Column>
 						<Column sizes={[['XS', 12], ['SM', 6]]}>
 							<h2>Buttons</h2>
+							<Row>
+								<Column>
+									<Button small transparent>Normal</Button>
+									<Button variant="primary" small>Primary</Button>
+									<Button variant="danger" small outline>Danger</Button>
+									<Button variant="dangerLight" small>Danger (Light)</Button>
+									<Button variant="success" small>Success</Button>
+									<Button variant="successLight" small>Success (Light)</Button>
+								</Column>
+							</Row>
+						</Column>
+					</Row>
+					<Row>
+						<Column sizes={[['XS', 12], ['SM', 6]]}>
+							<h2>Forms</h2>
+							<Label htmlFor="foo">Enter Name</Label>
+							<TextInput id="foo"></TextInput>
 						</Column>
 					</Row>
 				</Container>
