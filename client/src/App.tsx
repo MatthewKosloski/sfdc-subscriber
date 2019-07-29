6f830c6b326a5a685b9935c501cb25cb8c263cd3
Matthew Kosloski <matthewkosloski@me.com>
2019-07-28 23:09:06 -0500

    update app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,27 @@ import React from 'react';
 import Container from './design-system/layout/container';
 import { Row, Column } from './design-system/layout/grid';
 
+import Badge from './design-system/components/atoms/badge';
+
 const App: React.FC = () => {
 	return(
 		<Container>
 			<Row>
 				<Column sizes={[['XS', 12], ['SM', 6]]}>
-					<p>Hello world!</p>
+					<h2>Badges</h2>
+					<Row>
+						<Column>
+							<Badge small>Normal</Badge>
+							<Badge variant="primary" small>Primary</Badge>
+							<Badge variant="danger" small>Danger</Badge>
+							<Badge variant="dangerLight" small>Danger (Light)</Badge>
+							<Badge variant="success" small>Success</Badge>
+							<Badge variant="successLight" small>Success (Light)</Badge>
+						</Column>
+					</Row>
 				</Column>
 				<Column sizes={[['XS', 12], ['SM', 6]]}>
-					<p>Hello world!</p>
+					<h2>Buttons</h2>
 				</Column>
 			</Row>
 		</Container>
