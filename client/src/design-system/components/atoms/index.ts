b422217cf7cbcadfe7a9fe66c8a572cbfc476ecf
Matthew Kosloski <matthewkosloski@me.com>
2019-07-29 17:24:31 -0500

    added index to components/atoms

---
+++ client/src/design-system/components/atoms/index.ts
@@ -0,0 +1,11 @@
+import { default as Badge } from './badge';
+import { default as Button } from './Button';
+import { default as Label } from './Label';
+import { default as TextInput } from './TextInput';
+
+export default {
+    Badge,
+    Button,
+	Label,
+	TextInput
+};
