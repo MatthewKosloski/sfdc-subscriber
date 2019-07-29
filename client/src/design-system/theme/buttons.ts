459eab394d83ff7bd81bd0854360ac95ab9da988
Matthew Kosloski <matthewkosloski@me.com>
2019-07-29 12:37:24 -0500

    added props for button focus and hover

--- client/src/design-system/theme/buttons.ts
+++ client/src/design-system/theme/buttons.ts
@@ -1,10 +1,12 @@
 import colors from './colors';
 
-export type ButtonVariantStrings = 'normal' | 'primary' | 'danger' | 
+export type ButtonVariantStrings = 'normal' | 'primary' | 'danger' |
 'dangerLight' | 'success' | 'successLight';
 
 export interface IButtonProps {
-    variant?: ButtonVariantStrings,
+	variant?: ButtonVariantStrings,
+	transparent?: boolean,
+	selected?: boolean,
     outline?: boolean,
     heavy?: boolean,
     small?: boolean,
@@ -17,31 +19,43 @@ export const defaultProps: IButtonProps = {
 
 export const normal = {
 	backgroundColor: colors.neutralBlack,
+	backgroundColorHover: colors.shark,
+	borderColorFocus: colors.jumbo,
 	color: colors.white
 };
 
 export const primary = {
 	backgroundColor: colors.pantone306,
+	backgroundColorHover: colors.easternBlue,
+	borderColorFocus: colors.jordyBlue,
 	color: colors.white
 };
 
 export const danger = {
 	backgroundColor: colors.valencia,
+	backgroundColorHover: colors.tallPoppy,
+	borderColorFocus: colors.seaPink,
 	color: colors.white
 };
 
 export const dangerLight = {
 	backgroundColor: colors.cosmos,
+	backgroundColorHover: colors.cosmos,
+	borderColorFocus: colors.valencia,
 	color: colors.valencia
 };
 
 export const success = {
 	backgroundColor: colors.eucalyptus,
+	backgroundColorHover: colors.forestGreen,
+	borderColorFocus: colors.pastelGreen,
 	color: colors.white
 };
- 
+
 export const successLight = {
 	backgroundColor: colors.snowyMint,
+	backgroundColorHover: colors.snowyMint,
+	borderColorFocus: colors.eucalyptus,
 	color: colors.eucalyptus
 };
 
