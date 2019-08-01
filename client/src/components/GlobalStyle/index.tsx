0f0db3fa40d398cafd7d8c92e9c4093fa9da3ab3
Matthew Kosloski <matthewkosloski@me.com>
2019-08-01 15:27:12 -0500

    added design system util classes to global styles

--- client/src/components/GlobalStyle/index.tsx
+++ client/src/components/GlobalStyle/index.tsx
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 import reset from './reset';
+import utils from './utils';
 import typography from './typography';
 
 const GlobalStyle = createGlobalStyle`
 	${reset}
+	${utils}
 	${typography}
 `;
 
---
+++ client/src/components/GlobalStyle/utils.ts
@@ -0,0 +1,10 @@
+import designSystemUtils from '../../design-system/utils';
+
+const { align, font, display, spacing } = designSystemUtils;
+
+export default `
+	${align}
+	${display}
+	${font}
+	${spacing}
+`;
