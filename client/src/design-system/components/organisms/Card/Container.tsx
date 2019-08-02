760e865cb506ea06b96f3857869b8faa1c560ed5
Matthew Kosloski <matthewkosloski@me.com>
2019-08-02 11:50:55 -0500

    use html5 section tag for card container

--- client/src/design-system/components/organisms/Card/Container.tsx
+++ client/src/design-system/components/organisms/Card/Container.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { pxToEm } from '../../../abstracts';
 
-const Container = styled.div`
+const Container = styled.section`
 	width: 100%;
 	height: 100%;
 	display: flex;
