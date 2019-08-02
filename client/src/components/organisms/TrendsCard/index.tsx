8d6a3da044296dd9536122dfda34504d27161ff6
Matthew Kosloski <matthewkosloski@me.com>
2019-08-02 17:39:37 -0500

    begin adding list items to trends

--- client/src/components/organisms/TrendsCard/index.tsx
+++ client/src/components/organisms/TrendsCard/index.tsx
@@ -5,6 +5,9 @@ import { Card } from '../../../design-system/components';
 import TrendBadgeNegative from './TrendBadgeNegative';
 import TrendBadgePositive from './TrendBadgePositive';
 
+import { CardListItems } from '../';
+import CardListItem from './CardListItem';
+
 import Container from './Container';
 
 interface ITrendsCardProps {}
@@ -13,9 +16,26 @@ const TrendsCard: React.FC<ITrendsCardProps> = () => (
 	<Container>
 		<Card
 			titleText="Trends"
-			constrictBodyHeight>
-			<TrendBadgePositive count="12"/>
-			<TrendBadgeNegative count="125" />
+			fullBodyHeight
+			noPaddedBody>
+			<CardListItems>
+				<CardListItem
+					titleText="Product_License_Expiration__e"
+					subtitleText="for 1 minute"
+					borderLeftColor="rgba(43, 46, 106, 1)" />
+				<CardListItem
+					titleText="DataCenter_Exception__e"
+					subtitleText="for 2 minutes"
+					borderLeftColor="rgba(255, 193, 24, 1)" />
+				<CardListItem
+					titleText="DataCenter_Status_Change__e"
+					subtitleText="for 9 minutes"
+					borderLeftColor="rgba(0, 237, 188, 1)" />
+				<CardListItem
+					titleText="Client_Health_Change__e"
+					subtitleText="for 12 minutes"
+					borderLeftColor="rgba(149, 62, 189, 1)" />
+			</CardListItems>
 		</Card>
 	</Container>
 );
