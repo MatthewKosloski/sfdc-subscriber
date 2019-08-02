ad539f3775ee7895e2cd2ba082c4672f07afebf6
Matthew Kosloski <matthewkosloski@me.com>
2019-08-02 00:00:06 -0500

    added global body style

--- client/src/components/GlobalStyle/index.tsx
+++ client/src/components/GlobalStyle/index.tsx
@@ -2,11 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 
 import reset from './reset';
 import utils from './utils';
+import body from './body';
 import typography from './typography';
 
 const GlobalStyle = createGlobalStyle`
 	${reset}
 	${utils}
+	${body}
 	${typography}
 `;
 
