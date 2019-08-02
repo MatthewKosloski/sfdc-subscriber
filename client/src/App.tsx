eb84c0d8bafdeb84af9325958b791386bf572053
Matthew Kosloski <matthewkosloski@me.com>
2019-08-02 11:00:10 -0500

    updated app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import React, { Fragment, Component } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { Row, Column } from './design-system/layout';
-import { GlobalStyle, Header, Main, TimelineCard } from './components';
+import { GlobalStyle, Header, Main, TimelineCard,
+	SubscriptionsCard, TrendsCard, Sidebar } from './components';
 import theme from './theme';
 
 class App extends Component {
@@ -13,15 +14,33 @@ class App extends Component {
 				<GlobalStyle />
 				<ThemeProvider theme={theme}>
 					<Fragment>
-						<Header title="SFDC Subscriber"/>
-						<div className="u-display-flex">
+						<Header
+							title="SFDC Subscriber" />
+						<div
+							className="u-display-flex">
 							<Main>
-								<Row style={{minHeight: '60vh'}}>
+								<Row
+									style={{minHeight: '60vh'}}>
 									<Column>
 										<TimelineCard />
 									</Column>
 								</Row>
+								<Row
+									style={{minHeight: '40vh'}}>
+									<Column
+										sizes={[['XS', 12], ['LG', 6]]}
+										className="u-pr-zero">
+										<SubscriptionsCard />
+									</Column>
+									<Column
+										sizes={[['XS', 12], ['LG', 6]]}>
+										<TrendsCard />
+									</Column>
+								</Row>
 							</Main>
+							<Sidebar>
+								<p>Sidebar children</p>
+							</Sidebar>
 						</div>
 					</Fragment>
 				</ThemeProvider>
