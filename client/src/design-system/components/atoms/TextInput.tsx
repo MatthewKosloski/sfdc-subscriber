c7c0ef07d3208c6c43c4cbccd3e07fb4379f1d93
Matthew Kosloski <matthewkosloski@me.com>
2019-08-02 10:58:04 -0500

    fixed design system text input color

--- client/src/design-system/components/atoms/TextInput.tsx
+++ client/src/design-system/components/atoms/TextInput.tsx
@@ -11,14 +11,14 @@ const TextInput = styled.input.attrs({
 	display: block;
 	font-size: 1em;
 	border-radius: ${pxToEm(4)};
-	border: ${pxToEm(1)} solid ${({theme: {colors}}) => colors.gallery};
-	color: ${({theme: colors}) => colors.neutralBlack};
+	border: ${pxToEm(1)} solid ${({theme}) => theme.gallery};
+	color: ${({theme}) => theme.neutralBlack};
 	background-color: transparent;
 	transition-property: box-shadow;
 	transition-duration: 0.15s;
 	transition-timing-function: ease-in-out;
 	&:focus {
-		box-shadow: 0 0 0 ${pxToEm(3)} ${({theme: {colors}}) => colors.silver};
+		box-shadow: 0 0 0 ${pxToEm(3)} ${({theme}) => theme.silver};
 		outline: 0;
 	}
 `;
