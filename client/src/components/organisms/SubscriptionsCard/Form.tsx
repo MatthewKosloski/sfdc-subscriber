8a71baa45e2eab00329ac7b37f2f48761586ba3b
Matthew Kosloski <matthewkosloski@me.com>
2019-08-05 00:44:57 -0500

    changed subscriptions form from functional component to class

--- client/src/components/organisms/SubscriptionsCard/Form.tsx
+++ client/src/components/organisms/SubscriptionsCard/Form.tsx
@@ -1,21 +1,56 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import uuidv1 from 'uuid/v1';
 
 import { TextInput, Label, Button } from '../../../design-system/components';
 
-interface IFormProps {}
+interface IFormProps {
+	onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+}
 
-const Form: React.FC<IFormProps> = () => {
-	const textInputId: string = uuidv1();
-	return (
-		<Fragment>
-			<Label htmlFor={textInputId}>Event API Name</Label>
-			<div className="u-display-flex">
-				<TextInput id={textInputId} className="u-mr-one" />
-				<Button variant="success">Subscribe</Button>
-			</div>
-		</Fragment>
-	);
-};
+interface IStateProps {
+	value: string
+}
+
+class Form extends React.Component<IFormProps, IStateProps> {
+
+	private _textInputId: string;
+
+	constructor(props: IFormProps) {
+		super(props);
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+
+		this.state = { value: '' };
+
+		this._textInputId = uuidv1();
+	}
+
+	public handleInputChange(e: React.ChangeEvent<HTMLInputElement>): void {
+		this.setState({ value: e.target.value.toUpperCase() });
+	}
+
+	public render(): JSX.Element {
+		return (
+			<form onSubmit={this.props.onSubmit}>
+				<Label htmlFor={this._textInputId}>
+					Event API Name
+				</Label>
+				<div className="u-display-flex">
+					<TextInput 
+						className="u-mr-one"
+						id={this._textInputId} 
+						value={this.state.value}
+						onChange={this.handleInputChange} />
+					<Button 
+						type="submit" 
+						variant="success">
+						Subscribe
+					</Button>
+				</div>
+			</form>
+		);
+	}
+
+}
 
 export default Form;
