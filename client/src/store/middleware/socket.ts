09b2a703fc43e495bbcfb331c8e4683778b0d2fb
Matthew Kosloski <matthewkosloski@me.com>
2019-08-06 16:13:49 -0500

    temporarily disable socket middleware

--- client/src/store/middleware/socket.ts
+++ client/src/store/middleware/socket.ts
@@ -1,18 +1,10 @@
 import { MiddlewareAPI, Dispatch } from 'redux';
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 
 function socket() {
-	const socket: SocketIOClient.Socket = io('http://localhost:3001');
-
 	return (store: MiddlewareAPI) => (next: Dispatch) => (action: any) => {
 
-		const shouldSkipMiddleware: boolean = action.type !== 'ADD_SUBSCRIPTION';
-
-		if(shouldSkipMiddleware) {
-			return next(action);
-		}
-
-		return socket.emit(action.event, action.handle);
+		return next(action);
 	};
 }
 
