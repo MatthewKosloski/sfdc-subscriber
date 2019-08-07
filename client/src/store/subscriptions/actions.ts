a797ddb9db0006f74e4c2a82c17ea6916de156cb
Matthew Kosloski <matthewkosloski@me.com>
2019-08-07 17:46:11 -0500

    renamed/modified subscribe action to subscriptionRequest

--- client/src/store/subscriptions/actions.ts
+++ client/src/store/subscriptions/actions.ts
@@ -1,10 +1,11 @@
-import { 
-	Subscription, 
-	ADD_SUBSCRIPTION, 
-	REMOVE_SUBSCRIPTION, 
+import {
+	Subscription,
+	ADD_SUBSCRIPTION,
+	REMOVE_SUBSCRIPTION,
+	SUBSCRIPTION_REQUEST,
 	AddSubscriptionAction,
 	RemoveSubscriptionAction,
-	SubscribeAction 
+	SubscriptionRequestAction
 } from './types';
 
 export const addSubscription = (newSubscription: Subscription): AddSubscriptionAction => ({
@@ -12,21 +13,32 @@ export const addSubscription = (newSubscription: Subscription): AddSubscriptionA
 	payload: newSubscription
 });
 
-export const subscribe = (eventApiName: string): SubscribeAction => {
-	return {
-		event: 'PLATFORM_EVENT_SUBSCRIPTION_REQUEST',
-		args: {
-			payload: {
-				cometdChannel: `/event/${eventApiName}`,
-				socketEvent: 'PLATFORM_EVENT'
-			}
-		}
-	};
-};
-
 export const removeSubscription = (eventApiName: string): RemoveSubscriptionAction => ({
 	type: REMOVE_SUBSCRIPTION,
 	meta: {
 		eventApiName
 	}
 });
+
+export const subscriptionRequest = (eventApiName: string): SubscriptionRequestAction => {
+
+	const subscription: Subscription = {
+		eventApiName,
+		color: 'red',
+		minuteDuration: 0
+	};
+
+	return {
+		type: SUBSCRIPTION_REQUEST,
+		meta: {
+			socket: {
+				event: SUBSCRIPTION_REQUEST,
+				payload: {
+					successAction: addSubscription(subscription),
+					failureAction: null,
+					cometdChannel: `/event/${eventApiName}`
+				}
+			}
+		}
+	};
+};
