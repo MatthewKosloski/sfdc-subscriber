863dbb2c683835bb0c018e361f2070934d19e0c3
Matthew Kosloski <matthewkosloski@me.com>
2019-08-07 17:46:34 -0500

    updated store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,21 +1,28 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import io from 'socket.io-client';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { subscriptionsReducer } from './subscriptions/reducers';
 import { eventsReducer } from './events/reducers';
+import { toastReducer } from './toast/reducers';
 import { logger, socket } from './middleware';
 
 const rootReducer = combineReducers({
 	subscriptions: subscriptionsReducer,
-	events: eventsReducer
+	events: eventsReducer,
+	toasts: toastReducer
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
 
 export default function configureStore() {
 	const store = createStore(rootReducer, composeWithDevTools(
-		applyMiddleware(thunk, logger(), socket())
+		applyMiddleware(
+			thunk,
+			logger(),
+			socket(io('http://localhost:3001'))
+		)
 	));
 	return store;
 }
