a50bb40fe3538ff69b4cb4555a575d40c5d58721
Matthew Kosloski <matthewkosloski@me.com>
2019-08-08 13:10:56 -0500

    added RemoveAllEvents reducer

--- client/src/store/events/reducers.ts
+++ client/src/store/events/reducers.ts
@@ -2,8 +2,11 @@ import {
 	EventState,
 	EventActionTypes,
 	ADD_EVENT,
-	REMOVE_EVENT
+	REMOVE_EVENT,
+	REMOVE_ALL_EVENTS
 } from './types';
+
+import uuidv1 from 'uuid/v1';
 
 const initialState: EventState = [];
 
@@ -15,7 +18,7 @@ const initialState: EventState = [];
  * @param uuid The uuid to use for the check.
  */
 function hasEvent(state: EventState, uuid: string): boolean {
-	return state.some((event) => event.uuid === uuid);
+	return state.some((event) => event.Uuid === uuid);
 };
 
 export function eventsReducer(
@@ -25,21 +28,22 @@ export function eventsReducer(
 
 	switch(action.type) {
 		case ADD_EVENT: {
-			if(hasEvent(state, action.payload.uuid)) {
-				console.log(`Already have event ${action.payload.uuid}.`);
+			if(hasEvent(state, action.payload.Uuid)) {
 				return state;
 			} else {
-				console.log(state);
 				return [action.payload, ...state];
 			}
 		}
 		case REMOVE_EVENT: {
 			if(hasEvent(state, action.meta.uuid)) {
 				return state.filter((event) =>
-					event.uuid !== action.meta.uuid);
+					event.Uuid !== action.meta.uuid);
 			} else {
 				return state;
 			}
+		}
+		case REMOVE_ALL_EVENTS: {
+			return [];
 		}
 		default: {
 			return state;
