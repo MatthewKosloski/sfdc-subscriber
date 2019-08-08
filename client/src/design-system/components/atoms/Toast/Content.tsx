7f6f7f459ef6fa099040ddfb429cbfd4010afdd7
Matthew Kosloski <matthewkosloski@me.com>
2019-08-08 13:06:18 -0500

    remove break-word from toast content

--- client/src/design-system/components/atoms/Toast/Content.tsx
+++ client/src/design-system/components/atoms/Toast/Content.tsx
@@ -5,7 +5,6 @@ import { spacingEm } from '../../../abstracts';
 const Content = styled.span`
 	${spacingEm(['PL', 'PR'], 'One')}
 	${spacingEm(['PT', 'PB'], 'ThreeFourths')}
-	word-break: break-all;
 `;
 
 export default Content;
