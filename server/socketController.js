cd6a3b800c89fed18c50eaf3162ab389ae0027fa
Matthew Kosloski <matthewkosloski@me.com>
2019-08-08 16:39:11 -0500

    emit redux actions from server

--- server/socketController.js
+++ server/socketController.js
@@ -1,6 +1,8 @@
 const socketEvents = require('./socketEvents');
+const uuidv1 = require('uuid/v1');
 
 const {
+	REDUX_ACTION,
 	PLATFORM_EVENT_SUBSCRIPTION_REQUEST,
 	PLATFORM_EVENT_SUBSCRIPTION_SUCCESS,
 	PLATFORM_EVENT_SUBSCRIPTION_FAILURE,
@@ -8,30 +10,78 @@ const {
 	PLATFORM_EVENT_UNSUBSCRIPTION_SUCCESS,
 	PLATFORM_EVENT_UNSUBSCRIPTION_FAILURE
 } = socketEvents;
-    
+
+const _successToastAction = (message) => ({
+	type: 'ADD_TOAST',
+	payload: {
+		variant: 'success',
+		message
+	}
+});
+
+const _errorToastAction = (message) => ({
+	type: 'ADD_TOAST',
+	payload: {
+		variant: 'danger',
+		message
+	}
+});
+
+const _addEventAction = (payload) => ({
+	type: 'ADD_EVENT',
+	payload: {
+		Uuid: uuidv1(),
+		...payload
+	}
+});
+
+const _emitReduxAction = (socket, action) => {
+	socket.emit('REDUX_ACTION', action);
+};
+
+const _emitSuccessToast = (socket, message) => {
+	return _emitReduxAction(socket, _successToastAction(message));
+};
+
+const _emitErrorToast = (socket, message) => {
+	return _emitReduxAction(socket, _errorToastAction(message));
+};
+
+const _emitAddEventAction = (socket, payload) => {
+	return _emitReduxAction(socket, _addEventAction(payload));
+}
+
 module.exports = function(sfdcClient, socket) {
 
 	console.log(`Socket ${socket.id} has connected to the channel.`);
 
-	const handleSubscriptionRequest = ({payload: {cometdChannel, socketEvent}}) => {
-		
-		const callback = (payload) => {
-			socket.emit(socketEvent, {payload});
-			console.log(`Sent a(n) ${socketEvent} event to socket ${socket.id}.`);
+	const handleSubscriptionRequest = ({successAction, failureAction, cometdChannel}) => {
+
+		const callback = ({channel, data: { payload }}) => {
+			const { CreatedById, CreatedDate, ...customFields } = payload;
+
+			_emitAddEventAction(socket, {
+				EventApiName: channel.replace('/event/',''),
+				CreatedById,
+				CreatedDate,
+				CustomFields: {
+					...customFields
+				}
+			});
+
+			console.log(`Sent a(n) ${channel} event to socket ${socket.id}.`);
 		};
 
 		const subscribeCallback = ({successful, subscription}) => {
-			const payload = { successful, subscription };
-
 			if(successful) {
-				socket.emit(PLATFORM_EVENT_SUBSCRIPTION_SUCCESS, {payload});
+				_emitReduxAction(socket, successAction);
 				console.log(`Socket ${socket.id} is now subscribing to ${subscription}...`);
 			} else {
-				socket.emit(PLATFORM_EVENT_SUBSCRIPTION_FAILURE, {payload});
+				_emitReduxAction(socket, failureAction);
 				console.log(`Socket ${socket.id} failed to subscribe to ${subscription}.`);
 			}
 		};
-		
+
 		sfdcClient.subscribe(cometdChannel, callback, subscribeCallback);
 	};
 
@@ -65,8 +115,23 @@ module.exports = function(sfdcClient, socket) {
 		});
 	};
 
+	sfdcClient.onSuccessfulLogin = () => {
+		return _emitSuccessToast(socket, 'Successfully logged into Salesforce!');
+	};
+
+	sfdcClient.onFailedLogin = () => {
+		return _emitErrorToast(socket, 'Failed to log into Salesforce.');
+	};
+
+	sfdcClient.onSuccessfulHandshake = () => {
+		return _emitSuccessToast(socket, 'Successfully shook hands with the Salesforce CometD server!');
+	};
+
+	sfdcClient.onFailedHandshake = () => {
+		return _emitErrorToast(socket, 'Failed to shake hands with the Salesforce CometD server.');
+	};
+
 	socket.on(PLATFORM_EVENT_SUBSCRIPTION_REQUEST, handleSubscriptionRequest);
 	socket.on(PLATFORM_EVENT_UNSUBSCRIPTION_REQUEST, handleUnsubscriptionRequest);
 	socket.on('disconnect', handleDisconnect);
-
 };
