c9fdc387a0bc75cd4470d7875db9eb41aa9f2574
Matthew Kosloski <matthewkosloski@me.com>
2019-08-09 15:45:05 -0500

    added callbacks for SFDCClient and SocketController

--- server/index.js
+++ server/index.js
@@ -7,9 +7,25 @@ const io = require('socket.io')(server);
 const cometdLib = require('cometd');
 const jsforceLib = require('jsforce');
 
-const SFDCClient = require('./modules/SFDCClient');
+const {
+	PLATFORM_EVENT,
+	PLATFORM_EVENT_SUBSCRIPTION_REQUEST,
+	PLATFORM_EVENT_SUBSCRIPTION_SUCCESS,
+	PLATFORM_EVENT_SUBSCRIPTION_FAILURE,
+	PLATFORM_EVENT_UNSUBSCRIPTION_REQUEST,
+	PLATFORM_EVENT_UNSUBSCRIPTION_SUCCESS,
+	PLATFORM_EVENT_UNSUBSCRIPTION_FAILURE,
+	COMETD_HANDSHAKE_SUCCESS,
+	COMETD_HANDSHAKE_FAILURE,
+	SALESFORCE_LOGIN_SUCCESS,
+	SALESFORCE_LOGIN_FAILURE
+} = require('../common/socketEvents');
 
-const socketController = require('./socketController');
+const {
+	DISCONNECT
+} = require('./socketEvents');
+
+const { SFDCClient, SocketController } = require('./modules');
 
 const {
 	SFDC_SUBSCRIBER_CLIENT_ID: CLIENT_ID,
@@ -33,6 +49,66 @@ io.on('connection', (socket) => {
 		}
 	});
 
-	const client = new SFDCClient(cometd, jsforce, USERNAME, PASSWORD, API_VERSION, true);
-	socketController(client, socket);
+	// Create a new instance of SFDCClient to facilitate
+	// communication with Salesforce.
+	const sfdcClient = new SFDCClient(cometd, jsforce, USERNAME, PASSWORD, API_VERSION, true);
+
+	// Create a new instance of SocketController to facilitate
+	// communication with the client web app over web sockets.
+	const socketController = new SocketController(sfdcClient, socket, PLATFORM_EVENT_SUBSCRIPTION_REQUEST,
+		PLATFORM_EVENT_UNSUBSCRIPTION_REQUEST, DISCONNECT);
+
+	sfdcClient.onSuccessfulLogin = () => {
+		console.log(`Socket ${socket.id} successfully logged into Salesforce using jsforce.`);
+		socket.emit(SALESFORCE_LOGIN_SUCCESS);
+	};
+
+	sfdcClient.onFailedLogin = () => {
+		console.log(`Socket ${socket.id} failed to log into Salesforce using jsforce.`);
+		socket.emit(SALESFORCE_LOGIN_FAILURE);
+	};
+
+	sfdcClient.onSuccessfulHandshake = () => {
+		console.log(`Socket ${socket.id} successfully shook hands with the Salesforce CometD server.`);
+		socket.emit(COMETD_HANDSHAKE_SUCCESS);
+	};
+
+	sfdcClient.onFailedHandshake = () => {
+		console.log(`Socket ${socket.id} failed to shake hands with the Salesforce CometD server.`);
+		socket.emit(COMETD_HANDSHAKE_FAILURE);
+	};
+
+	socketController.onEvent = (data) => {
+		console.log(`Sent a Salesforce Platform Event to socket ${socket.id}.`);
+		socket.emit(PLATFORM_EVENT, data);
+	};
+
+	socketController.onSuccessfulSubscription = (data) => {
+		console.log(`Socket ${socket.id} successfully subscribed to ${data.cometdChannel}!`);
+		socket.emit(PLATFORM_EVENT_SUBSCRIPTION_SUCCESS, data);
+	};
+
+	socketController.onFailedSubscription = (data) => {
+		console.log(`Socket ${socket.id} failed to subscribe to ${data.cometdChannel}.`);
+		socket.emit(PLATFORM_EVENT_SUBSCRIPTION_FAILURE, data);
+	};
+
+	socketController.onSuccessfulUnsubscription = (data) => {
+		console.log(`Socket ${socket.id} successfully unsubscribed from the CometD channel.`);
+		socket.emit(PLATFORM_EVENT_UNSUBSCRIPTION_SUCCESS, data);
+	};
+
+	socketController.onFailedUnsubscription = (data) => {
+		console.log(`Socket ${socket.id} failed to unsubscribe from the CometD channel.`);
+		socket.emit(PLATFORM_EVENT_UNSUBSCRIPTION_FAILURE, data);
+	};
+
+	socketController.onSuccessfulCometdDisconnect = () => {
+		console.log(`Socket ${socket.id} successfully disconnected from CometD.`);
+	};
+
+	socketController.onFailedCometdDisconnect = () => {
+		console.log(`Socket ${socket.id} failed to disconnect from CometD.`);
+	};
+
 });
