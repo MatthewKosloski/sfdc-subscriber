c59ddf686d736108a7ad6023ae149d3893950331
Matthew Kosloski <matthewkosloski@me.com>
2019-08-09 08:49:41 -0500

    redux-webpack initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+  	entry: './src/index.tsx',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.svg$/,
+				loader: 'svg-inline-loader'
+			}
+    	]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+ 	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: 'src/index.html'
+		})
+  	],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	}
+};
