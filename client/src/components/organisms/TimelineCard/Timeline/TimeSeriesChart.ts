ffdfcc9b18e3bcd4f30259848355a61449c196ef
Matthew Kosloski <matthewkosloski@me.com>
2019-08-12 14:15:37 -0500

    added options arg to constructor

--- client/src/components/organisms/TimelineCard/Timeline/TimeSeriesChart.ts
+++ client/src/components/organisms/TimelineCard/Timeline/TimeSeriesChart.ts
@@ -1,6 +1,16 @@
 import * as d3 from 'd3';
 
-interface Margin {
+interface Options {
+	circlesGroupClass: string,
+	circleTransitionDuration: number,
+	duration: number,
+	margin: Spacing,
+	outerHeight: number,
+	outerWidth: number,
+	xAxisTickDuration: number
+}
+
+interface Spacing {
     top: number,
     right: number,
     bottom: number,
@@ -9,85 +19,92 @@ interface Margin {
 
 class TimeSeriesChart {
 
-    private _svg: d3.Selection<SVGSVGElement, unknown, null, undefined>;
-    private _actualWidth: number;
-    private _actualHeight: number;
-    private _width: number;
-    private _height: number;
-    private _margin: Margin;
+	private _innerHeight: number;
+	private _options: Options;
+	private _svg: d3.Selection<SVGSVGElement, unknown, null, undefined>;
     private _xScale: d3.ScaleTime<number, number>;
     private _xAxis: d3.Axis<number | Date | { valueOf(): number; }>;
-    private _duration: number;
-    private _xAxisTickDuration: number;
 
-    constructor(node: any, width: number = 960, height: number = 500, duration: number = 5000, 
-        xAxisTickDuration: number = 75, margin: Margin = { top: 40, right: 40, bottom: 40, left: 40 }) {
-        this._svg = d3.select(node);
-        this._actualWidth = width;
-        this._actualHeight = height;
-        this._margin = margin;
-        this._width = this._actualWidth - this._margin.left - this._margin.right;
-        this._height = this._actualHeight - this._margin.top - this._margin.bottom;
-        this._xScale = d3.scaleTime().range([0, this._width]);
-        this._xAxis = d3.axisBottom(this._xScale);
-        this._duration = duration;
-        this._xAxisTickDuration = xAxisTickDuration;
+    constructor(node: any, options: Options = {
+		circlesGroupClass: 'chart__circles',
+		circleTransitionDuration: 250,
+		duration: 60000,
+		margin: {top: 0, right: 0, bottom: 20, left: 0},
+		outerHeight: 400,
+		outerWidth: 381,
+		xAxisTickDuration: 75
+	}) {
+
+		this._svg = d3.select(node);
+		this._options = options;
+
+		const { outerHeight, margin } = options;
+
+		this._innerHeight = outerHeight - margin.top - margin.bottom;
+        this._xScale = d3.scaleTime().range([0, this._innerHeight]);
+		this._xAxis = d3.axisBottom(this._xScale);
     }
 
     public init(): void {
-        this._svg
-            .attr('width', this._width + this._margin.left + this._margin.right)
-            .attr('height', this._height + this._margin.top + this._margin.bottom);
 
-        const chart = this._svg.append('g')
-            .attr('transform', `translate(${this._margin.left}, ${this._margin.top})`);
-    
-        chart.append('g')
-            .attr('class', 'chart__circles');
+		const { outerWidth, outerHeight, margin, duration,
+			xAxisTickDuration, circlesGroupClass } = this._options;
 
-        const xAxisGroup = chart.append('g')	
-            .attr('transform', `translate(0, ${this._height})`);	
+		this._svg.attr('viewBox', `0 0 ${outerWidth} ${outerHeight}`);
 
-        d3.timer(() => {	
-            const now = Date.now();	
-            this._xScale.domain([now - this._duration, now]);	
-            xAxisGroup.transition()	
-                .duration(this._xAxisTickDuration)	
-                .ease(d3.easeLinear)	
-                .call(this._xAxis);	
-        });	
+		const chart = this._svg.append('g')
+			.attr('transform', `translate(${margin.left}, 0)`);
+
+		chart.append('g')
+			.attr('class', circlesGroupClass)
+			.attr('transform', `translate(-${margin.left}, -${margin.top})`);
+
+        const xAxisGroup = chart.append('g')
+            .attr('transform', `translate(0, ${this._innerHeight})`);
+
+        d3.timer(() => {
+            const now = Date.now();
+            this._xScale.domain([now - duration, now]);
+            xAxisGroup.transition()
+                .duration(xAxisTickDuration)
+                .ease(d3.easeLinear)
+                .call(this._xAxis);
+        });
     }
 
-    public appendCircle(fill: string = 'black', y: number = Math.random() * this._height, 
-    time: number = Date.now(), radius: number = 12): void {
+    public appendCircle(fill: string = 'black', yMultiple: number = Math.random() * this._options.outerHeight,
+    time: number = Date.now(), diameter: number = 16): void {
+
+		const { duration, circlesGroupClass, circleTransitionDuration } = this._options;
 
         const now: number = Date.now();
         const timeDiff: number = now - time;
 
-        if(timeDiff > this._duration) {
+        if(timeDiff > duration) {
             return;
-        }
+		}
 
-        const circlesGroup = this._svg.select('g.chart__circles');
+		const radius = diameter / 2;
+        const circlesGroup = this._svg.select(`g.${circlesGroupClass}`);
 
         const circle = circlesGroup.append('circle')
             .attr('r', 0)
             .attr('stroke-opacity', 0)
             .attr('fill', fill)
-            .attr('cy', y);
-    
+            .attr('cy', yMultiple * diameter);
+
         circle.transition('time')
-            .duration(this._duration)
+            .duration(duration)
             .ease(d3.easeLinear)
             .attrTween('cx', () => () => String(this._xScale(time)));
-    
+
         circle.transition()
-            .duration(250)
+            .duration(circleTransitionDuration)
             .ease(d3.easeCubicOut)
             .attr('r', radius)
             .attr('stroke-opacity', 1)
         .transition()
-            .delay(this._duration - timeDiff - (250 * 2))
+            .delay(duration - timeDiff - (circleTransitionDuration * 2))
             .ease(d3.easeCubicIn)
             .attr('r', 0)
             .attr('stroke-opacity', 0)
