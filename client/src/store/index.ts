5e9f42e5c549446e1f7c90e8a9fa08f2c9e43de7
Matthew Kosloski <matthewkosloski@me.com>
2019-08-13 18:21:46 -0500

    re-enabled socketMiddleware, included addToastMiddleware

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,15 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import io from 'socket.io-client';
+import io from 'socket.io-client';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import entitiesReducer from './entities';
-import { 
-	loggerMiddleware, 
-	/*socketMiddlware,*/ 
+import {
+	loggerMiddleware,
+	socketMiddlware,
 	addEventMiddleware,
-	addSubscriptionMiddleware
+	addSubscriptionMiddleware,
+	addToastMiddleware
 } from './middleware';
-// import socketController from './socketController';
+import socketController from './socketController';
 
 const rootReducer = combineReducers({
 	entities: entitiesReducer
@@ -18,17 +19,18 @@ const rootReducer = combineReducers({
 export type AppState = ReturnType<typeof rootReducer>;
 
 export default function configureStore() {
-	// const socket = io('http://localhost:3001');
+	const socket = io('http://localhost:3001');
 	const store = createStore(rootReducer, composeWithDevTools(
 		applyMiddleware(
 			loggerMiddleware(),
 			addEventMiddleware(),
-			addSubscriptionMiddleware()
-			// socketMiddlware(socket)
+			addSubscriptionMiddleware(),
+			addToastMiddleware(),
+			socketMiddlware(socket)
 		)
 	));
 
-	// socketController(socket, store.dispatch);
+	socketController(socket, store.dispatch);
 
 	return store;
 }
