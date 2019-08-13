36d9a84f1ec3a439f7a36c1ce9949f899f64b69d
Matthew Kosloski <matthewkosloski@me.com>
2019-08-13 18:18:02 -0500

    deleted client utils

--- client/src/utils/createSubscription.ts
+++
@@ -1,8 +0,0 @@
-import { Subscription } from '../store/entities/subscriptions/types';
-import randomColor from './randomColor';
-
-export default (eventApiName: string): Subscription => ({
-	color: randomColor(),
-	eventApiName: eventApiName.replace('/event/', ''),
-	minuteDuration: 0
-});
