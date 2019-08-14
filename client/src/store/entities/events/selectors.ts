6ff5dc9ef874ab11abae1b932a6245b5c2a9cc0c
Matthew Kosloski <matthewkosloski@me.com>
2019-08-14 16:22:18 -0500

    created new event selectors

--- client/src/store/entities/events/selectors.ts
+++ client/src/store/entities/events/selectors.ts
@@ -1,8 +1,6 @@
 import { AppState } from '../../';
 import { Event } from '../events/types';
 
-import { lastArrayElement } from '../../../utils';
-
 export const selectEvents = (state: AppState): Event[] => {
 	return Object.values(state.entities.events.byId);
 };
@@ -17,11 +15,41 @@ export const selectEventKeysBySubscriptionId = (subscriptionId: string) => (stat
 	return eventsWithSubscriptionId.map((event) => event.id) as string[];
 };
 
-export const selectEventById = (id: string) => (state: AppState): Event => {
-	return state.entities.events.byId[id];
+export const selectEventById = (id: string | null) => (state: AppState): Event | null => {
+	if(state.entities.events.byId && id) {
+		return state.entities.events.byId[id];
+	} else {
+		return null;
+	}
 };
 
-export const selectLastEvent = (state: AppState): Event => {
-	const lastEventId: string = lastArrayElement(state.entities.events.allIds);
+export const selectLastEvent = (state: AppState): Event | null => {
+	const lastEventId: string = state.entities.events.lastId;
 	return selectEventById(lastEventId)(state);
-}
+};
+
+export const selectLastEventId = (state: AppState): string | null => {
+	if(state.entities.events.lastId) {
+		return state.entities.events.lastId;
+	} else {
+		return null;
+	}
+};
+
+export const selectLastEventSubscriptionId = (state: AppState): string | null => {
+	const lastEvent = selectLastEvent(state);
+	return lastEvent && lastEvent.subscriptionId
+		? lastEvent.subscriptionId
+		: null;
+};
+
+export const selectEventCreatedDateById = (state: AppState, id: string | null): string | null => {
+	if(id) {
+		const event = selectEventById(id)(state);
+		return event && event.createdDate
+			? event.createdDate
+			: null;
+	} else {
+		return null;
+	}
+};
