7826347aa39f7e681ade78c32840e6a20966d166
Matthew Kosloski <matthewkosloski@me.com>
2019-08-14 16:21:46 -0500

    check for null cases

--- client/src/store/entities/subscriptions/selectors.ts
+++ client/src/store/entities/subscriptions/selectors.ts
@@ -1,10 +1,30 @@
 import { AppState } from '../../';
 import { Subscription } from './types';
 
-export const selectSubscriptions = (state: AppState): Subscription[] => {
-	return Object.values(state.entities.subscriptions);
+export const selectSubscriptions = (state: AppState): Subscription[] | null => {
+	if(state.entities.subscriptions) {
+		return Object.values(state.entities.subscriptions);
+	} else {
+		return null;
+	}
 };
 
-export const selectSubscriptionById = (state: AppState, id: string): Subscription => {
-	return state.entities.subscriptions[id];
+export const selectSubscriptionById = (state: AppState, id: string | null): Subscription | null => {
+	if(id && state.entities.subscriptions[id]) {
+		return state.entities.subscriptions[id];
+	} else {
+		return null;
+	}
+};
+
+export const selectSubscriptionColorById = (state: AppState, subscriptionId: string | null): string | null => {
+	if(subscriptionId) {
+		const subscription = selectSubscriptionById(state, subscriptionId);
+
+		return subscription && subscription.color
+			? subscription.color
+			: null;
+	} else {
+		return null;
+	}
 };
